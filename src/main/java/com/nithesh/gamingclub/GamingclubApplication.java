4aff0b8a0514e81287673cde28705bd0413fa7f8
nitheshgowda10 <nitheshgowda28@gmail>
2025-09-23 14:09:10 +0530

    Initial Commit

---
+++ src/main/java/com/nithesh/gamingclub/GamingclubApplication.java
@@ -0,0 +1,13 @@
+package com.nithesh.gamingclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingclubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithesh/gamingclub/GamingclubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithesh.gamingclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingclubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
